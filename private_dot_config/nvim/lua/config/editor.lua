03040fb1b5f4a9b65bd663f1d2587485a1c0679f
Noah Wager <noahwager@gmail.com>
2025-12-20 23:39:54 -0800

    nvim: Add editor settings and ftplugin
    Signed-off-by: Noah Wager <noahwager@gmail.com>

---
+++ private_dot_config/nvim/lua/config/editor.lua
@@ -0,0 +1,11 @@
+-- Disable mouse
+vim.opt.mouse = ""
+
+-- Disable editorconfig
+vim.g.editorconfig = false
+
+-- Set default indent
+vim.opt.autoindent = true
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 8
+vim.opt.tabstop = 8
